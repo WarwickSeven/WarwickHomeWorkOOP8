9c6cb3915b73ff8cd9fd5ffbc617938bbc1b0562
WarwickSeven <WarwickSeven@gmail.com>
2022-04-25 16:14:50 +0300

    Lesson H Task 1

--- WarwickHomeWorkOOP8/main.cpp
+++ WarwickHomeWorkOOP8/main.cpp
@@ -1,1 +1,20 @@
+//Lesson H Task 1
+#include <iostream>
 
+double division(double const &a, double const &b) {
+    if (b==0)
+        throw "DivisionByZero";
+    return a/b;
+}
+
+int main() {
+    double a = 10;
+    double b = 4;
+    try {
+        std::cout << division(a,b) << std::endl;
+        } catch (const char* e) {
+            std::cerr << e << std::endl;
+        }
+    return 0;
+}
+ 
