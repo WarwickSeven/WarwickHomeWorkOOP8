0016e58d906c0cb50ecae175485eff2a286e6c24
WarwickSeven <WarwickSeven@gmail.com>
2022-04-25 18:10:12 +0300

    Lesson H Task 2 (try 1)

--- WarwickHomeWorkOOP8/main.cpp
+++ WarwickHomeWorkOOP8/main.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
 
+class Ex {
+public:
+    double x;
+    Ex(const double &x) {
+        this->x = x;
+        throw "Error. y+a > 100";
+    }
+};
+
+class Bar {
+    double y;
+public:
+    Bar(double y = 0) {
+        this->y=y;
+    }
+    void setBar(double a) {
+        if (y+a> 100) {
+            throw(Ex(a*y));
+        } else {
+            y = a;
+        }
+    }
+};
+
+int main() {
+    Bar p1;
+    double n;
+    do {
+        std::cout << "n: ";
+        std::cin >> n;
+       try {
+           p1.setBar(n);
+       } catch (const char * a) {
+           std::cerr << a << std::endl;
+       }
+        
+    } while (n!=0);
+    return 0;
+}
